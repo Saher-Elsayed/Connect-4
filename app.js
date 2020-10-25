53187e203764111676a24f0dcc4c3c80296a5199
Saher Elsayed <saher.suga@gmail.com>
2020-10-24 20:57:06 -0500

    finished Phase 1: intiallizing the names of the players

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 
-let tableRow =document.getElementsByTagName("tr");
-let tableCol =document.getElementsByTagName("td");
-let cells = document.querySelector(".cell");
+var tableRow =document.getElementsByTagName("tr");
+var tableCol =document.getElementsByTagName("td");
+var tableCells =document.querySelector(".tableCell");
 
-let playerTurn=document.querySelector(".playerTurn");
-let reset =document.querySelector(".reset")
+const playerTurn=document.querySelector(".playerTurn");
+const reset =document.querySelector(".reset")
 
 // Testing for click detections
 for(let i=0; i<tableCol.length; i++)
 {
     console.log("I am HERE")
     tableCol[i].addEventListener("click", (e) =>{
-        console.log(`${e.target.parentElement.rowIndex}, ${e.target.parentElement.cellIndex}`)
+    console.log(`${e.target.parentElement.rowIndex}, ${e.target.parentElement.cellIndex}`)
     });
 };
+
+p2Color="blue";
+P1Color="red";
+
+
+//adding names (Phase I)
+    while(!player1){
+        var player1=prompt("Player 1 name:")
+    }
+    while(!player2){
+        var player2=prompt("player 2 name:")
+    }
+    console.log(player1,player2)
+
+//switching turns(Phase II)
+let currentPlayer=1;
+playerTurn.textContent=`${player1}'s turn`;
